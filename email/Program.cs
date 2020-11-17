70d672274095e5c709aee1865f2f87fd63e45d08
Niklas <niklas.ostman02@gmail.com>
2020-11-17 14:21:48 +0100

    First commit

---
+++ email/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System;
+using System.Net.Mail;
+
+namespace email
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                var smtpClient = new SmtpClient("smtp.gmail.com"){
+                Port = 587,
+                Credentials = new NetworkCredential("testet69420@gmail.com", "Jagvetinte852"),
+                EnableSsl = true,
+            };
+            
+            
+                smtpClient.Send("testet69420@gmail.com", "testet69420@gmail.com", "","");   
+            
+        
+        }
+    }
+}
